7938702842602e9727fae89c30ce6b3c0e9db434
Alvin Philips <alvinphilips257@gmail.com>
2023-11-29 17:28:24 -0500

    feat: add math (#16)
    * Added Vec2 :)
    
    * Added Distance because Roy asked nicely

---
+++ Engine/Engine/Math.cpp
@@ -0,0 +1,132 @@
+#include "Math.h"
+
+Vec2 Vec2::Zero = Vec2();
+Vec2 Vec2::UnitX = Vec2(1, 0);
+Vec2 Vec2::UnitY = Vec2(0, 1);
+
+Vec2::Vec2() {
+    x = 0;
+    y = 0;
+}
+
+Vec2::Vec2(float value) {
+    x = value;
+    y = value;
+}
+
+Vec2::Vec2(float _x, float _y) {
+    x = _x;
+    y = _y;
+}
+
+Vec2 Vec2::operator-() const {
+    return Vec2(-this->x, -this->y);
+}
+
+Vec2& Vec2::operator+=(const Vec2 &rhs) {
+    this->x += rhs.x;
+    this->y += rhs.y;
+
+    return *this;
+}
+
+Vec2& Vec2::operator-=(const Vec2 &rhs) {
+    this->x -= rhs.x;
+    this->y -= rhs.y;
+
+    return *this;
+}
+
+Vec2& Vec2::operator*=(const Vec2 &rhs) {
+    this->x *= rhs.x;
+    this->y *= rhs.y;
+
+    return *this;
+}
+
+Vec2& Vec2::operator/=(const Vec2 &rhs) {
+    this->x /= rhs.x;
+    this->y /= rhs.y;
+
+    return *this;
+}
+
+Vec2& Vec2::operator*=(const float &f) {
+    this->x *= f;
+    this->y *= f;
+
+    return *this;
+}
+
+Vec2& Vec2::operator/=(const float &f) {
+    float inv_f = 1.0f / f;
+    this->x *= inv_f;
+    this->y *= inv_f;
+
+    return *this;
+}
+
+Vec2 Vec2::operator+(const Vec2 &rhs) const
+{
+    return Vec2(this->x + rhs.x, this->y + rhs.y);
+}
+
+Vec2 Vec2::operator-(const Vec2 &rhs) const
+{
+    return Vec2(this->x - rhs.x, this->y - rhs.y);
+}
+
+Vec2 Vec2::operator*(const Vec2 &rhs) const {
+    return Vec2(this->x * rhs.x, this->y * rhs.y);
+}
+
+Vec2 Vec2::operator/(const Vec2 &rhs) const {
+    return Vec2(this->x / rhs.x, this->y / rhs.y);
+}
+
+Vec2 Vec2::operator*(const float &f) const
+{
+    return Vec2(this->x * f, this->y * f);
+}
+
+Vec2 Vec2::operator/(const float &f) const
+{
+    float inv_f = 1.0f / f;
+    return Vec2(this->x * inv_f, this->y * inv_f);
+}
+
+void Vec2::Normalize()
+{
+    // TODO: Maybe rework this check?
+    if (this->x == 0 || this-> y == 0) {
+        return;
+    }
+    *this /= this->Magnitude();
+}
+
+float Vec2::Magnitude() const
+{
+    return SDL_sqrtf(this->MagnitudeSquared());
+}
+
+float Vec2::Dot(const Vec2 &rhs) const
+{
+    return this->x * rhs.x + this->y * rhs.y;
+}
+
+float Vec2::Dot(const Vec2 &lhs, const Vec2 &rhs) {
+    return lhs.x * rhs.x + lhs.y * rhs.y;
+}
+
+float Vec2::MagnitudeSquared() const
+{
+    return this->x * this->x + this->y * this->y;
+}
+
+float Vec2::Distance(const Vec2 &rhs) const {
+    return (*this - rhs).Magnitude();
+}
+
+float Vec2::Distance(const Vec2 &lhs, const Vec2 &rhs) {
+    return (lhs - rhs).Magnitude();
+}
---
+++ Engine/Engine/Math.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#ifndef _MATH_H_
+#define _MATH_H_
+
+#include <SDL_rect.h>
+
+typedef union Vec2
+{
+    struct {
+        float x;
+        float y;
+    };
+    SDL_FPoint data;
+
+    Vec2();
+    Vec2(float value);
+    Vec2(float _x, float _y);
+    static Vec2 Zero;
+    static Vec2 UnitX;
+    static Vec2 UnitY;
+    Vec2 operator-() const;
+    Vec2& operator+=(const Vec2 &rhs);
+    Vec2& operator-=(const Vec2 &rhs);
+
+    // Cursed shader-programmer zone starts
+    Vec2& operator*=(const Vec2 &rhs);
+    Vec2& operator/=(const Vec2 &rhs);
+    // Cursed shader-programmer zone ends
+
+    Vec2& operator*=(const float &f);
+    Vec2& operator/=(const float &f);
+
+    Vec2 operator+(const Vec2 &rhs) const;
+    Vec2 operator-(const Vec2 &rhs) const;
+    
+    // Cursed shader-programmer zone starts
+    Vec2 operator*(const Vec2 &rhs) const;
+    Vec2 operator/(const Vec2 &rhs) const;
+    // Cursed shader-programmer zone ends
+
+    Vec2 operator*(const float &f) const;
+    Vec2 operator/(const float &f) const;
+
+    float Magnitude() const;
+    float MagnitudeSquared() const;
+    void Normalize();
+    float Dot(const Vec2 &rhs) const;
+    static float Dot(const Vec2 &lhs, const Vec2 &rhs);
+
+    // Only because Roy asked me to :(
+    float Distance(const Vec2 &rhs) const;
+    static float Distance(const Vec2 &lhs, const Vec2 &rhs);
+};
+
+// Exercise for the viewer :)
+typedef union IVec2
+{
+    struct {
+        int x;
+        int y;
+    };
+    SDL_Point data;
+};
+
+#endif
