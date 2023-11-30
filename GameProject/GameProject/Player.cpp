1bc49f0aa146ee27cbfc0050cbcac91a1e9d82f0
bennieb1 <97978644+bennieb1@users.noreply.github.com>
2023-11-30 16:07:29 -0500

    fixed player waiting for transform and vec class

--- GameProject/GameProject/Player.cpp
+++ GameProject/GameProject/Player.cpp
@@ -1,100 +1,34 @@
 #include "GameCore.h"
 #include "Player.h"
-Player::Player(float x, float y, float speed)
-	: posX(x), posY(y), speed(speed), health(100), score(0) {
-}
+#include <EngineTime.h>
 
-Player::~Player() {
-	// Cleanup if needed
-}
 
-void Player::update(const InputSystem& inputSystem) {
-	// Keyboard movement with WASD and arrow keys
-	SDL_Event event;
-	while (SDL_PollEvent(&event)) {
-		if (event.type == SDL_CONTROLLERDEVICEADDED) {
-			int joystickIndex = event.cdevice.which;
-			SDL_GameController* gamepad = SDL_GameControllerOpen(joystickIndex);
-			// Assign an ID to this gamepad, like the index or a unique identifier
-			gamepadId = joystickIndex; // This is a simplification
-		}
-		if (inputSystem.isKeyPressed(SDLK_LEFT) || inputSystem.isKeyPressed(SDLK_a)) {
-			moveLeft();
-		}
-		if (inputSystem.isKeyPressed(SDLK_RIGHT) || inputSystem.isKeyPressed(SDLK_d)) {
-			moveRight();
-		}
-		if (inputSystem.isKeyPressed(SDLK_UP) || inputSystem.isKeyPressed(SDLK_w)) {
-			moveUp();
-		}
-		if (inputSystem.isKeyPressed(SDLK_DOWN) || inputSystem.isKeyPressed(SDLK_s)) {
-			moveDown();
-		}
+void Player::Update(const InputSystem& inputSystem)
+{
+	    Transform& player = entity->transform;
+    Vec2 dir = Vec2.Zero;
+    InputSystem& input = InputSystem::Instance();
 
-		// Gamepad movement
-		if (inputSystem.isGamepadButtonPressed(gamepadId, SDL_CONTROLLER_BUTTON_DPAD_LEFT) ||
-			inputSystem.getGamepadAxisState(gamepadId, SDL_CONTROLLER_AXIS_LEFTX) < -gamepadDeadzone) {
-			moveLeft();
-		}
-		if (inputSystem.isGamepadButtonPressed(gamepadId, SDL_CONTROLLER_BUTTON_DPAD_RIGHT) ||
-			inputSystem.getGamepadAxisState(gamepadId, SDL_CONTROLLER_AXIS_LEFTX) > gamepadDeadzone) {
-			moveRight();
-		}
-		if (inputSystem.isGamepadButtonPressed(gamepadId, SDL_CONTROLLER_BUTTON_DPAD_UP) ||
-			inputSystem.getGamepadAxisState(gamepadId, SDL_CONTROLLER_AXIS_LEFTY) < -gamepadDeadzone) {
-			moveUp();
-		}
-		if (inputSystem.isGamepadButtonPressed(gamepadId, SDL_CONTROLLER_BUTTON_DPAD_DOWN) ||
-			inputSystem.getGamepadAxisState(gamepadId, SDL_CONTROLLER_AXIS_LEFTY) > gamepadDeadzone) {
-			moveDown();
-		}
+    if (input.isKeyPressed(SDLK_LEFT) || input.isKeyPressed(SDLK_a) || input.isGamepadButtonPressed(0, SDL_CONTROLLER_BUTTON_DPAD_LEFT)) {
+        dir.x -= 1;
+    }
+    if (input.isKeyPressed(SDLK_RIGHT) || input.isKeyPressed(SDLK_d) || input.isGamepadButtonPressed(0, SDL_CONTROLLER_BUTTON_DPAD_RIGHT)) {
+        dir.x += 1;
+    }
+    if (input.isKeyPressed(SDLK_UP) || input.isKeyPressed(SDLK_w) || input.isGamepadButtonPressed(0, SDL_CONTROLLER_BUTTON_DPAD_UP)) {
+        dir.y -= 1;
+    }
+    if (input.isKeyPressed(SDLK_DOWN) || input.isKeyPressed(SDLK_s) || input.isGamepadButtonPressed(0, SDL_CONTROLLER_BUTTON_DPAD_DOWN)) {
+        dir.y += 1;
+    }
 
-		updatePhysics();
-		handleCollisions();
-	}
-}
+    if (dir == Vec2.Zero) {
+        dir.x = input.getGamepadAxisState(0, SDL_CONTROLLER_AXIS_LEFTX);
+        dir.y = input.getGamepadAxisState(0, SDL_CONTROLLER_AXIS_LEFTY);
+    }
+    if (dir != Vec2.Zero) {
+        dir.Normalize();
+    }
 
-void Player::render() {
-	// Render the player using your graphics system
-	// Example: drawPlayer(posX, posY); 
-}
-
-void Player::moveLeft() {
-	posX -= speed;
-}
-
-void Player::moveRight() {
-	posX += speed;
-}
-
-void Player::moveUp() {
-	posY -= speed;
-}
-
-void Player::moveDown() {
-	posY += speed;
-}
-
-float Player::getX() const {
-	return posX;
-}
-
-float Player::getY() const {
-	return posY;
-}
-
-void Player::setX(float x) {
-	posX = x;
-}
-
-void Player::setY(float y) {
-	posY = y;
-}
-
-void Player::handleCollisions() {
-	// Collision handling logic
-}
-
-void Player::updatePhysics() {
-	// Update player physics, like gravity
+    player.pos += dir * speed * Time::Instance().DeltaTime();;
 }
