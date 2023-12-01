e5cbfb0285547375bbe8a6fccf1d1e78a9cd653b
Hylu <roylu99@gmail.com>
2023-12-01 11:48:19 -0500

    Add docstrings

--- Engine/Engine/AnimatedSprite.h
+++ Engine/Engine/AnimatedSprite.h
@@ -4,6 +4,12 @@
 #define _ANIMATED_SPRITE_H_
 
 #include "Sprite.h"
+
+/**
+ * @class AnimatedSprite
+ *
+ * AnimatedSprite is a derived class of Sprite that renders an animation using a spritesheet
+ */
 class AnimatedSprite : public Sprite {
 	DECLARE_DYNAMIC_DERIVED_CLASS(AnimatedSprite, Sprite);
 public:
@@ -17,34 +23,78 @@ public:
 	int totalFrames = 0;
 	int defaultFrameNumber = 0;
 
+	int currentFrame = 0;
 	float frameDelay = 0.1f;
-	float frameCounter = 0;
 
 private:
-	int currentFrame = 0;
+	float frameCounter = 0;
 	bool running = true;
 	bool loop = true;
 
+	/**
+	 * @brief Initializes any variables as needed
+	 *
+	 */
 	void Initialize();
 
+	/**
+	 * @brief Cleans up any memory and pointers as needed
+	 *
+	 */
 	void Destroy();
 
+	/**
+	 * @brief Updates the animation frame to render based off of game time
+	 *
+	 */
 	void Update();
 
+	/**
+	 * @brief Copies the sprite onto a renderer to be displayed for that frame
+	 *
+	 */
 	void Render() override;
 
 public:
+	/**
+	 * @brief AnimatedSprite constructor
+	 *
+	 */
 	AnimatedSprite();
+
+	/**
+	 * @brief AnimatedSprite destructor
+	 *
+	 */
 	~AnimatedSprite();
 
+	/**
+	 * @brief Setter for how to parse the spritesheet
+	 *
+	 * @param rows How many rows from the spritesheet to parse
+	 * @param cols How many columns from the spritesheet to parse
+	 * @param frames How many frames/sprites make up this animation
+	 */
 	void SetSpriteSheet(int rows, int cols, int frames);
 
+	/**
+	 * @brief Restart the current animation frame to 0
+	 */
 	void Restart();
 
+	/**
+	 * @brief Stops the animation, reverting to the default frame
+	 */
 	void Stop();
 
+	/**
+	 * @brief Plays the animation from the current frame
+	 */
 	void Play();
 
+	/**
+	 * @brief Pauses the animation on the current frame
+	 */
 	void Pause();
 };
 
--- Engine/Engine/Sprite.h
+++ Engine/Engine/Sprite.h
@@ -6,6 +6,11 @@
 #include "SDL.h"
 #include "Renderable.h"
 
+/**
+ * @class Sprite
+ *
+ * Sprite is a derived class of Renderable and serves to render a single texture on screen
+ */
 class Sprite : public Renderable
 {
 	DECLARE_DYNAMIC_DERIVED_CLASS(Sprite, Renderable);
@@ -15,20 +20,55 @@ protected:
 	SDL_Rect sourceRect = { 0,0,0,0 };
 	SDL_Rect targetRect = { 0,0,0,0 };
 
+	/**
+	 * @brief Initializes any variables as needed
+	 *
+	 */
 	void Initialize();
 
+	/**
+	 * @brief Cleans up any memory and pointers as needed
+	 *
+	 */
 	void Destroy();
 
+	/**
+	 * @brief Updates the sprite class
+	 *
+	 */
 	void Update();
 
+	/**
+	 * @brief Copies the sprite onto a renderer to be displayed for that frame
+	 *
+	 */
 	void Render() override;
 
 public:
+	/**
+	 * @brief Sprite constructor
+	 *
+	 */
 	Sprite();
+
+	/**
+	 * @brief Sprite destructor
+	 *
+	 */
 	~Sprite();
 
+	/**
+	 * @brief Setter for the texture area to render
+	 *
+	 * @param _rect A rect of four values describing the area of the texture to render
+	 */
 	void SetSourceRect(SDL_Rect _rect);
 
+	/**
+	 * @brief Initializes the sprite class
+	 *
+	 * @param _texture A pointer to an SDL texture for this class to render
+	 */
 	void SetNewTexture(SDL_Texture* _texture);
 	
 };
